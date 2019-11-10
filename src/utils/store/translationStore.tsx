0b437f1f878b89c74e50ee261b01d09cbc6bc04e
piotr.zielinski <piotr.zielinski@sidnet.info>
2019-11-10 11:19:14 +0100

    Add detecting browser's language

--- src/utils/store/translationStore.tsx
+++ src/utils/store/translationStore.tsx
@@ -1,8 +1,13 @@
 import translations from 'utils/translation/translations';
-import { observable, computed } from 'mobx';
+import { observable } from 'mobx';
+import detectBrowserLanguage from 'detect-browser-language';
 
 export class TranslationStore {
-  @observable private lang = 'en';
+  @observable private lang: string;
+
+  constructor(defaultLang: string) {
+    this.lang = defaultLang;
+  }
 
   public setLang(lang: string): void {
     this.lang = lang;
@@ -12,11 +17,14 @@ export class TranslationStore {
     return this.lang;
   }
 
-  getProperText(key: string): string {
+  public getProperText(key: string): string {
     return translations[this.getLang()][key];
   }
 }
 
-const translationStore = new TranslationStore();
+const browserLang = detectBrowserLanguage();
+const defaultLang = browserLang in translations ? browserLang : 'en';
+
+const translationStore = new TranslationStore(defaultLang);
 
 export default translationStore;
