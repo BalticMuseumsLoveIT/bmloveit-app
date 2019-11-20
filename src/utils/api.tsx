787388b89151d579c9ee678a7fcb1503505c6cf1
Michał Zbytniewski <michal@zbytniewski.com>
2019-11-20 14:49:23 +0100

    Add GET methods for quiz endpoint

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,5 +1,10 @@
 import userStore from 'utils/store/userStore';
 import axios from 'axios';
+import {
+  QuizDetailsInterface,
+  QuizDetailsNotFound,
+  QuizInterface,
+} from './@types/interfaces';
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
@@ -47,6 +52,34 @@ abstract class Api {
     } catch (error) {
       return Promise.reject(error);
     }
+  }
+
+  /**
+   * Get list of all active quizzes.
+   * Response will not include questions.
+   *
+   * @throws Axios error
+   */
+  public static async getQuizList(): Promise<QuizInterface[]> {
+    const response = await axiosInstance.get('quiz');
+    return response.data;
+  }
+
+  /**
+   * Get quiz details.
+   * Response will include quiz base data and questions list.
+   *
+   * If quiz is not active, has been deleted or was not created yet,
+   * {QuizDetailsNotFound} object will be returned.
+   *
+   * @param {number} id - Quiz ID
+   * @throws Axios error
+   */
+  public static async getQuiz(
+    id: number,
+  ): Promise<QuizDetailsInterface | QuizDetailsNotFound> {
+    const response = await axiosInstance.get(`quiz/?id=${id}`);
+    return response.data;
   }
 }
 
