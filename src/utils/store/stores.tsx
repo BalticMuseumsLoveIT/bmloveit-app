2e1f3023e146e37b7745cfc73713500e0811f8a7
Michał Zbytniewski <michal@zbytniewski.com>
2019-11-21 15:21:50 +0100

    Add QuizStore reference

--- src/utils/store/stores.tsx
+++ src/utils/store/stores.tsx
@@ -1,11 +1,13 @@
 import userStore from './userStore';
 import uiStore from './uiStore';
 import routesStore from './routesStore';
+import quizStore from './quizStore';
 
 const stores = {
   userStore,
   uiStore,
   routesStore,
+  quizStore,
 };
 
 export default stores;
