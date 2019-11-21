7ccc8a68807a3d9af698bd776c17b420cd369414
Michał Zbytniewski <michal@zbytniewski.com>
2019-11-21 15:43:06 +0100

    Populate list of active quizzes

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,5 +1,6 @@
 import { QuizStore } from 'utils/store/quizStore';
 import Content from 'components/Content/Content';
+import Api from 'utils/api';
 import React from 'react';
 import Helmet from 'react-helmet';
 import { inject, observer } from 'mobx-react';
@@ -11,6 +12,13 @@ interface Props {
 @inject('quizStore')
 @observer
 class QuizPage extends React.Component<Props> {
+  componentDidMount(): void {
+    Api.getQuizList().then(response => {
+      this.props.quizStore.quizList = response;
+      // TODO: Handle errors
+    });
+  }
+
   render() {
     return (
       <>
