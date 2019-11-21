2eaedf27a0e87a98737598c62f7cbed31b8fd44a
Michał Zbytniewski <michal@zbytniewski.com>
2019-11-21 17:09:51 +0100

    Render list of active quizzes

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -20,12 +20,21 @@ class QuizPage extends React.Component<Props> {
   }
 
   render() {
+    const list = this.props.quizStore.quizList.map(quiz => (
+      <p key={quiz.id}>
+        <a href={`/quiz/${quiz.id}`}>{quiz.name}</a>
+      </p>
+    ));
+
     return (
       <>
         <Helmet>
           <title>Quiz</title>
         </Helmet>
-        <Content>Content</Content>
+        <Content>
+          <h1>List of active quizzes</h1>
+          {list}
+        </Content>
       </>
     );
   }
