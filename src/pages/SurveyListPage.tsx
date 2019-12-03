3c5bf09432cd89209a18dc4cb58b7f9ee2802d51
Michał Zbytniewski <michal@zbytniewski.com>
2019-12-03 15:46:54 +0100

    Add survey list functionality

--- src/pages/SurveyListPage.tsx
+++ src/pages/SurveyListPage.tsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
+import { observer } from 'mobx-react';
+import { Link } from 'react-router-dom';
 import Content from '../components/Content/Content';
+import store, { SurveyListState } from '../utils/store/surveyListStore';
 
+@observer
 class SurveyListPage extends Component {
+  async componentDidMount(): Promise<void> {
+    await store.loadList();
+  }
+
   render() {
+    const list = function() {
+      switch (store.state) {
+        case SurveyListState.LOADING:
+          return <p>Wczytywanie...</p>;
+        case SurveyListState.LOADED:
+          return store.list.map(survey => (
+            <p key={survey.id}>
+              <Link to={`/survey/${survey.id}`}>{survey.name}</Link>
+            </p>
+          ));
+        case SurveyListState.ERROR:
+          return <p>Wystąpił błąd podczas pobierania listy ankiet</p>;
+        default:
+          return null;
+      }
+    };
+
     return (
       <>
         <Helmet>
@@ -11,6 +36,7 @@ class SurveyListPage extends Component {
         </Helmet>
         <Content>
           <h1>List of active surveys</h1>
+          {list()}
         </Content>
       </>
     );
--- src/utils/store/surveyListStore.tsx
+++ src/utils/store/surveyListStore.tsx
@@ -1,8 +1,36 @@
-import { observable } from 'mobx';
+import { action, computed, observable } from 'mobx';
 import { SurveyInterface } from '../interfaces';
+import Api from '../api';
 
+export enum SurveyListState {
+  NOT_LOADED,
+  LOADING,
+  LOADED,
+  ERROR,
+}
 export class SurveyListStore {
-  @observable surveyList: SurveyInterface[] = [];
+  @observable private _state: SurveyListState = SurveyListState.NOT_LOADED;
+
+  @computed get state() {
+    return this._state;
+  }
+
+  @observable private _list: SurveyInterface[] = [];
+
+  @computed get list() {
+    return this._list;
+  }
+
+  @action async loadList() {
+    this._state = SurveyListState.LOADING;
+    try {
+      this._list = await Api.getSurveyList();
+      this._state = SurveyListState.LOADED;
+    } catch (e) {
+      this._state = SurveyListState.ERROR;
+      // TODO: Handle error
+    }
+  }
 }
 
 const surveyListStore = new SurveyListStore();
