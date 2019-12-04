a4a47fe5da5df4dfd4ad6d1f20876a75101d5bcd
Michał Zbytniewski <michal@zbytniewski.com>
2019-12-04 18:10:26 +0100

    Add basic logic of survey loading

--- src/pages/SurveyDetailsPage.tsx
+++ src/pages/SurveyDetailsPage.tsx
@@ -1,24 +1,46 @@
 import Content from 'components/Content/Content';
-import store from 'utils/store/surveyDetailsStore';
+import store, { SurveyDetailsState } from 'utils/store/surveyDetailsStore';
 import React, { Component } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import { observer } from 'mobx-react';
 
+const Survey = function(props: { state: SurveyDetailsState }) {
+  switch (props.state) {
+    case SurveyDetailsState.LOADING:
+      return <p>Wczytywanie...</p>;
+    case SurveyDetailsState.LOADED:
+      return <p>Details</p>;
+    case SurveyDetailsState.NOT_FOUND:
+      return <p>Ankieta o podanym identyfikatorze nie została odnaleziona</p>;
+    case SurveyDetailsState.ERROR:
+      return <p>Wystąpił błąd podczas pobierania ankiety</p>;
+    default:
+      return null;
+  }
+};
+
+interface Props extends RouteComponentProps<any> {}
+
 @observer
-class SurveyDetailsPage extends Component {
-  componentDidMount() {
-    console.log(store.state);
+class SurveyDetailsPage extends Component<Props> {
+  async componentDidMount() {
+    const {
+      params: { id },
+    } = this.props.match;
+
+    await store.loadSurvey(id);
   }
 
   render() {
     return (
       <>
         <Helmet>
-          <title>Survey list</title>
+          <title>Survey details</title>
         </Helmet>
         <Content>
           <h1>Survey details</h1>
-          <p>Content</p>
+          <Survey state={store.state} />
         </Content>
       </>
     );
--- src/utils/store/surveyDetailsStore.tsx
+++ src/utils/store/surveyDetailsStore.tsx
@@ -1,15 +1,46 @@
-import { computed, observable } from 'mobx';
+import { isAPIError, SurveyDetailsInterface } from 'utils/interfaces';
+import Api from 'utils/api';
+import { action, computed, observable } from 'mobx';
 
 export enum SurveyDetailsState {
   NOT_LOADED,
+  LOADING,
+  LOADED,
+  NOT_FOUND,
+  ERROR,
 }
 
 export class SurveyDetailsStore {
   @observable private _state: SurveyDetailsState =
     SurveyDetailsState.NOT_LOADED;
 
-  @computed get state() {
+  @computed get state(): SurveyDetailsState {
     return this._state;
+  }
+
+  @observable private _survey: SurveyDetailsInterface | null = null;
+
+  @computed get survey() {
+    return this._survey;
+  }
+
+  @action async loadSurvey(id: number) {
+    this._state = SurveyDetailsState.LOADING;
+    try {
+      this._survey = await Api.getSurveyDetails(id);
+      this._state = SurveyDetailsState.LOADED;
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status === 404 &&
+        isAPIError(error.response.data)
+      ) {
+        this._state = SurveyDetailsState.NOT_FOUND;
+      } else {
+        this._state = SurveyDetailsState.ERROR;
+        // TODO: Handle unexpected error
+      }
+    }
   }
 }
 
