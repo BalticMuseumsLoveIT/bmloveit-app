f520f349cc7696ad8a25435a48978834b5caf0b3
Michał Zbytniewski <michal@zbytniewski.com>
2019-12-04 15:25:42 +0100

    Add getPrivateMedia method

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -55,6 +55,16 @@ abstract class Api {
     }
   }
 
+  public static async getPrivateMedia(path: string): Promise<string> {
+    const response = await axiosInstance.get(path, {
+      responseType: 'arraybuffer',
+    });
+
+    const data = Buffer.from(response.data, 'binary').toString('base64');
+
+    return `data:${response.headers['content-type']};base64,${data}`;
+  }
+
   /**
    * Get list of all active quizzes.
    * Response will not include questions.
