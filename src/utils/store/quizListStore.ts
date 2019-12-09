8979b7339dc08f9c6a1bd16ec445659afe00cbc8
Michał Zbytniewski <michal@zbytniewski.com>
2019-12-09 11:27:58 +0100

    Add quiz list specific store

---
+++ src/utils/store/quizListStore.ts
@@ -0,0 +1,38 @@
+import { QuizInterface } from 'utils/interfaces';
+import Api from 'utils/api';
+import { action, computed, observable } from 'mobx';
+
+export enum QuizListState {
+  NOT_LOADED,
+  LOADING,
+  LOADED,
+  ERROR,
+}
+
+export class QuizListStore {
+  @observable private _state: QuizListState = QuizListState.NOT_LOADED;
+
+  @computed get state() {
+    return this._state;
+  }
+
+  @observable private _list: Array<QuizInterface> = [];
+
+  @computed get list() {
+    return this._list;
+  }
+
+  @action async loadList() {
+    this._state = QuizListState.LOADING;
+    try {
+      this._list = await Api.getQuizList();
+      this._state = QuizListState.LOADED;
+    } catch (e) {
+      this._state = QuizListState.ERROR;
+    }
+  }
+}
+
+const quizListStore = new QuizListStore();
+
+export default quizListStore;
