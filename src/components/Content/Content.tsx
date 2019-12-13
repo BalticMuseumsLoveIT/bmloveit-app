1921bef7604e1f8922b7567658910b4151614e07
Michał Zbytniewski <michal@zbytniewski.com>
2019-12-13 13:48:12 +0100

    Handle descendant errors

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -16,28 +16,38 @@ interface Props {
 
 @observer
 class Content extends React.Component<Props> {
+  state = { hasDescendantError: false };
+
   render() {
     let contentToRender: React.ReactNode;
 
-    switch (this.props.state) {
-      case ContentState.NOT_LOADED:
-        contentToRender = null;
-        break;
-      case ContentState.LOADING:
-        contentToRender = <h1>LOADING</h1>;
-        break;
-      case ContentState.LOADED:
-      default:
-        // If content state is loaded or undefined
-        contentToRender = this.props.children;
-        break;
-      case ContentState.ERROR:
-        contentToRender = <h1>ERROR</h1>;
-        break;
+    if (this.state.hasDescendantError) {
+      contentToRender = <h1>Descendant Error</h1>;
+    } else {
+      switch (this.props.state) {
+        case ContentState.ERROR:
+          contentToRender = <h1>Error</h1>;
+          break;
+        case ContentState.NOT_LOADED:
+          contentToRender = null;
+          break;
+        case ContentState.LOADING:
+          contentToRender = <h1>LOADING</h1>;
+          break;
+        case ContentState.LOADED:
+        default:
+          // If content state is loaded or undefined
+          contentToRender = this.props.children;
+          break;
+      }
     }
 
     return <StyledWrapper>{contentToRender}</StyledWrapper>;
   }
+
+  static getDerivedStateFromError = () => {
+    return { hasDescendantError: true };
+  };
 }
 
 export default Content;
