6010383b787e06b37c35f233e76227b7ea4beaf3
Michał Zbytniewski <michal@zbytniewski.com>
2019-12-14 14:39:46 +0100

    Add optional Content State handling

--- src/utils/store/quizListStore.ts
+++ src/utils/store/quizListStore.ts
@@ -1,6 +1,8 @@
 import { QuizInterface } from 'utils/interfaces';
 import Api from 'utils/api';
-import { action, observable } from 'mobx';
+import { action, autorun, observable } from 'mobx';
+import uiStore from './uiStore';
+import { ContentState } from '../../components/Content/Content';
 
 export enum QuizListState {
   NOT_LOADED,
@@ -11,8 +13,26 @@ export enum QuizListState {
 
 export default class QuizListStore {
   @observable state: QuizListState = QuizListState.NOT_LOADED;
-
   @observable list: Array<QuizInterface> = [];
+
+  private _manageContentState = () => {
+    switch (this.state) {
+      case QuizListState.LOADING:
+        uiStore.setContentState(ContentState.PROCESSING);
+        break;
+      case QuizListState.NOT_LOADED:
+      case QuizListState.LOADED:
+      case QuizListState.ERROR:
+      default:
+        uiStore.setContentState(ContentState.AVAILABLE);
+    }
+  };
+
+  constructor(manageContentState = false) {
+    if (manageContentState) {
+      autorun(this._manageContentState);
+    }
+  }
 
   @action setState = (state: QuizListState) => {
     this.state = state;
