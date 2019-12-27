0de05044d487ab92f2d9bf48b4bbc15d4bd227aa
Michał Zbytniewski <michal@zbytniewski.com>
2019-12-27 18:11:17 +0100

    Add minimal styling

--- src/components/Footer/Footer.style.tsx
+++ src/components/Footer/Footer.style.tsx
@@ -3,6 +3,24 @@ import styled from 'styled-components';
 const StyledWrapper = styled.div`
   display: flex;
   align-items: center;
+  margin: 1em 0;
+
+  a {
+    display: inline-block;
+    padding: 0.5em;
+    margin-right: 0.5em;
+    border: 1px solid thistle;
+    border-radius: 3px;
+    text-decoration: none;
+  }
+
+  a:hover {
+    background: paleturquoise;
+  }
+
+  a:last-child {
+    margin-right: 0;
+  }
 `;
 
 export default StyledWrapper;
