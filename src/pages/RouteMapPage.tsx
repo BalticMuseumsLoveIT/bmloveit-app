a01699585045c66e8283a545c78744637f5a29c4
Michał Zbytniewski <michal@zbytniewski.com>
2019-12-27 17:58:06 +0100

    Add route map page part 2

---
+++ src/pages/RouteMapPage.tsx
@@ -0,0 +1,71 @@
+import Content from 'components/Content/Content';
+import { UiStore } from 'utils/store/uiStore';
+import RouteMapPageStore from 'utils/store/routeMapPageStore';
+import Footer from 'components/Footer/Footer';
+import React from 'react';
+import Helmet from 'react-helmet';
+import { inject, observer } from 'mobx-react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { withTranslation, WithTranslation } from 'react-i18next';
+
+interface Props extends WithTranslation, RouteComponentProps<any> {
+  uiStore: UiStore;
+}
+
+@inject('uiStore')
+@observer
+class RouteMapPage extends React.Component<Props> {
+  routeMapPageStore = new RouteMapPageStore(true);
+
+  async componentDidMount(): Promise<void> {
+    const {
+      params: { id },
+    } = this.props.match;
+
+    this.routeMapPageStore.setTReady(this.props.tReady);
+
+    await this.routeMapPageStore.loadData(Number.parseInt(id));
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.tReady !== this.props.tReady) {
+      // Inform state about translation status
+      this.routeMapPageStore.setTReady(this.props.tReady);
+    }
+  }
+
+  componentWillUnmount(): void {
+    // Reset `<Content />` state
+    this.routeMapPageStore.unmount();
+  }
+
+  render() {
+    if (!this.props.tReady) return null;
+
+    return (
+      <>
+        <Helmet>
+          <title>{this.props.t('page.title', 'Route Map')}</title>
+        </Helmet>
+        <Content>
+          <h1>{this.props.t('content.title', 'Route map')}</h1>
+          <h2>{this.routeMapPageStore.routeTitle}</h2>
+          <div>Map Component</div>
+          <Footer>
+            <Link to={`/area/${this.routeMapPageStore.routeId}/routes`}>
+              {this.props.t('button.changeRoute.label', 'Change route')}
+            </Link>
+            <Link to={`/route/${this.routeMapPageStore.routeId}/locations`}>
+              {this.props.t('button.viewList.label', 'View list')}
+            </Link>
+            <Link to={`/qrcode`}>
+              {this.props.t('button.scanQR.label', 'Scan QR')}
+            </Link>
+          </Footer>
+        </Content>
+      </>
+    );
+  }
+}
+
+export default withTranslation('route-map-page')(RouteMapPage);
---
+++ src/utils/store/routeMapPageStore.ts
@@ -0,0 +1,84 @@
+import { RouteInterface } from 'utils/interfaces';
+import uiStore from 'utils/store/uiStore';
+import { ContentState } from 'components/Content/Content';
+import Api from 'utils/api';
+import { action, autorun, computed, observable, when } from 'mobx';
+
+export enum PageState {
+  NOT_LOADED,
+  LOADING,
+  LOADED,
+  ERROR,
+}
+
+export default class AreaRoutesPageStore {
+  private readonly _manageContentState: boolean;
+
+  @observable state: PageState = PageState.NOT_LOADED;
+  @observable routeData: RouteInterface | null = null;
+  @observable tReady?: boolean;
+
+  private _handleContentState = () => {
+    switch (this.state) {
+      case PageState.LOADING:
+        uiStore.setContentState(ContentState.PROCESSING);
+        break;
+      case PageState.NOT_LOADED:
+      case PageState.LOADED:
+      case PageState.ERROR:
+      default:
+        uiStore.setContentState(ContentState.AVAILABLE);
+    }
+  };
+
+  constructor(manageContentState = false) {
+    this._manageContentState = manageContentState;
+
+    if (manageContentState) {
+      autorun(this._handleContentState);
+    }
+  }
+
+  @action loadData = async (routeId: number) => {
+    try {
+      this.setState(PageState.LOADING);
+
+      const [routeData] = await Promise.all([
+        Api.getRoute(routeId),
+        // Keep `PROCESSING` state till translations are fetched
+        when(() => this.tReady === true),
+      ]);
+
+      this.setRouteData(routeData);
+      this.setState(PageState.LOADED);
+    } catch (e) {
+      this.setState(PageState.ERROR);
+    }
+  };
+
+  @action setState = (state: PageState) => {
+    this.state = state;
+  };
+
+  @action setTReady(tReady?: boolean) {
+    this.tReady = tReady;
+  }
+
+  @action setRouteData = (routesData: RouteInterface) => {
+    this.routeData = routesData;
+  };
+
+  @computed get routeId(): string {
+    return this.routeData ? this.routeData.id.toString() : '';
+  }
+
+  @computed get routeTitle(): string {
+    return this.routeData ? this.routeData.name_full : '';
+  }
+
+  @action unmount = () => {
+    if (this._manageContentState) {
+      this.setState(PageState.NOT_LOADED);
+    }
+  };
+}
