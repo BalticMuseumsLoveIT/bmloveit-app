d7c116b6c7f4f823cb8e86af19589a1d184b09f7
Michał Zbytniewski <michal@zbytniewski.com>
2020-01-02 13:15:16 +0100

    Prettier

--- src/components/SurveyForm/QuestionImage.tsx
+++ src/components/SurveyForm/QuestionImage.tsx
@@ -1,7 +1,13 @@
 import { getPrivateMediaURL } from 'utils/helpers';
 import React from 'react';
 
-export const QuestionImage = ({path, alt }: { path?: string; alt?: string }) => {
+export const QuestionImage = ({
+  path,
+  alt,
+}: {
+  path?: string;
+  alt?: string;
+}) => {
   return path && path.length ? (
     <div>
       <img src={getPrivateMediaURL(path)} alt={alt || ''} />
