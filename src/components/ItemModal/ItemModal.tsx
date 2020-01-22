ef6680e0f452847a7e609937f29020fb288b2929
Michał Zbytniewski <michal@zbytniewski.com>
2020-01-22 18:35:26 +0100

    Add close button

--- src/components/ItemModal/ItemModal.tsx
+++ src/components/ItemModal/ItemModal.tsx
@@ -2,6 +2,7 @@ import { ItemHtmlParser } from 'components/ItemHtmlParser/ItemHtmlParser';
 import { getPrivateMediaURL } from 'utils/helpers';
 import ItemModalStore, { ModalState } from 'utils/store/itemModalStore';
 import ReactModal from 'react-modal';
+import { useTranslation } from 'react-i18next';
 import 'components/ItemModal/ItemModal.css';
 import { observer } from 'mobx-react';
 import React from 'react';
@@ -11,57 +12,69 @@ interface ItemModalProps {
 }
 
 export const ItemModal = observer(({ store }: ItemModalProps) => {
+  const { t, ready } = useTranslation();
+
   return (
-    <ReactModal {...store.modalProps}>
-      {(() => {
-        switch (store.modalState) {
-          case ModalState.NOT_LOADED:
-            return null;
-          case ModalState.LOADING:
-            return <h1>Loading...</h1>;
-          case ModalState.NOT_FOUND:
-            return <h1>Not found</h1>;
-          case ModalState.ERROR:
-            return <h1>Error</h1>;
-          case ModalState.LOADED:
-          default:
-            return (
-              <>
-                {store.item.itemNameFull && <h1>{store.item.itemNameFull}</h1>}
-                {store.item.itemDescription && (
-                  <ItemHtmlParser html={store.item.itemDescription} />
-                )}
-                {store.item.itemImage && (
-                  <div>
-                    <img
-                      src={getPrivateMediaURL(store.item.itemImage.file_url)}
-                      alt=""
-                    />
-                  </div>
-                )}
-                {store.item.itemAudio && (
-                  <div>
-                    <audio controls id="audio_player">
-                      <source
-                        src={getPrivateMediaURL(store.item.itemAudio.file_url)}
-                        type="audio/mpeg"
+    (ready && (
+      <ReactModal {...store.modalProps}>
+        <button onClick={store.modalProps.onRequestClose}>
+          {t('button.modalClose.label', 'Close')}
+        </button>
+        {(() => {
+          switch (store.modalState) {
+            case ModalState.NOT_LOADED:
+              return null;
+            case ModalState.LOADING:
+              return <h1>Loading...</h1>;
+            case ModalState.NOT_FOUND:
+              return <h1>Not found</h1>;
+            case ModalState.ERROR:
+              return <h1>Error</h1>;
+            case ModalState.LOADED:
+            default:
+              return (
+                <>
+                  {store.item.itemNameFull && (
+                    <h1>{store.item.itemNameFull}</h1>
+                  )}
+                  {store.item.itemDescription && (
+                    <ItemHtmlParser html={store.item.itemDescription} />
+                  )}
+                  {store.item.itemImage && (
+                    <div>
+                      <img
+                        src={getPrivateMediaURL(store.item.itemImage.file_url)}
+                        alt=""
                       />
-                    </audio>
-                  </div>
-                )}
-                {store.item.itemVideo && (
-                  <div>
-                    <video
-                      controls
-                      id="video_player"
-                      src={getPrivateMediaURL(store.item.itemVideo.file_url)}
-                    />
-                  </div>
-                )}
-              </>
-            );
-        }
-      })()}
-    </ReactModal>
+                    </div>
+                  )}
+                  {store.item.itemAudio && (
+                    <div>
+                      <audio controls id="audio_player">
+                        <source
+                          src={getPrivateMediaURL(
+                            store.item.itemAudio.file_url,
+                          )}
+                          type="audio/mpeg"
+                        />
+                      </audio>
+                    </div>
+                  )}
+                  {store.item.itemVideo && (
+                    <div>
+                      <video
+                        controls
+                        id="video_player"
+                        src={getPrivateMediaURL(store.item.itemVideo.file_url)}
+                      />
+                    </div>
+                  )}
+                </>
+              );
+          }
+        })()}
+      </ReactModal>
+    )) ||
+    null
   );
 });
