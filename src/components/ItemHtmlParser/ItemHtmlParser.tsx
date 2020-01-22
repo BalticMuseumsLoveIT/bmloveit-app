ee0af148ada5d199f9832eee7c2d5f2ea06267d9
Michał Zbytniewski <michal@zbytniewski.com>
2020-01-22 16:56:12 +0100

    Add ItemHtmlParser component

---
+++ src/components/ItemHtmlParser/ItemHtmlParser.tsx
@@ -0,0 +1,26 @@
+import { DomElement } from 'htmlparser2';
+import { Link } from 'react-router-dom';
+import ReactHtmlParser, { processNodes } from 'react-html-parser';
+import React from 'react';
+
+interface ItemHtmlParserProps {
+  html: string;
+}
+
+export const ItemHtmlParser = ({ html }: ItemHtmlParserProps) => {
+  const transform = (node: DomElement) => {
+    if (node.type === 'tag' && node.name === 'a') {
+      return (
+        <Link to={(node.attribs && node.attribs.href) || ''}>
+          {processNodes(node.children || [], transform)}
+        </Link>
+      );
+    }
+  };
+
+  return (
+    <React.Fragment>
+      {ReactHtmlParser(html, { transform: transform })}
+    </React.Fragment>
+  );
+};
