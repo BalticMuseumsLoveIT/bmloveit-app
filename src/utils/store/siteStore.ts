0e7a909ca5febc0225167f197e1ef8ec26f27766
Michał Zbytniewski <michal@zbytniewski.com>
2020-01-31 11:45:08 +0100

    Add global site store

---
+++ src/utils/store/siteStore.ts
@@ -0,0 +1,58 @@
+import { SiteInterface } from 'utils/interfaces';
+import Api from 'utils/api';
+import { getTranslatedString } from 'utils/helpers';
+import { action, computed, observable } from 'mobx';
+
+export class SiteStore {
+  @observable siteData: SiteInterface | null = null;
+
+  isDataAvailable(): this is { siteData: SiteInterface } {
+    return this.siteData !== null;
+  }
+
+  @action setSiteData = (
+    siteData: SiteInterface | Array<SiteInterface> | null,
+  ) => {
+    if (Array.isArray(siteData)) {
+      siteData = siteData.length > 0 ? siteData[0] : null;
+    }
+
+    this.siteData = siteData;
+  };
+
+  @action loadSiteData = async () => {
+    const response = await Api.getSiteData();
+    this.setSiteData(response);
+  };
+
+  @computed get title(): string {
+    if (!this.isDataAvailable()) return '';
+
+    return getTranslatedString(
+      this.siteData.title,
+      this.siteData.title_translation,
+    );
+  }
+
+  @computed get description(): string {
+    if (!this.isDataAvailable()) return '';
+
+    return getTranslatedString(
+      this.siteData.description,
+      this.siteData.description_translation,
+    );
+  }
+
+  @computed get about(): string {
+    if (!this.isDataAvailable()) return '';
+
+    return getTranslatedString(
+      this.siteData.about,
+      this.siteData.about_translation,
+    );
+  }
+}
+
+const siteStore = new SiteStore();
+
+export default siteStore;
