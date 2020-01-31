36c6038ad8df8f16b064178e10da7e05d0a91c44
Michał Zbytniewski <michal@zbytniewski.com>
2020-01-31 11:57:37 +0100

    Add siteStore to global stores Fix paths

--- src/utils/store/stores.ts
+++ src/utils/store/stores.ts
@@ -1,13 +1,15 @@
-import userStore from './userStore';
-import userProfileStore from './userProfileStore';
-import uiStore from './uiStore';
-import cookieBarStore from './cookieBarStore';
+import siteStore from 'utils/store/siteStore';
+import userStore from 'utils/store/userStore';
+import userProfileStore from 'utils/store/userProfileStore';
+import uiStore from 'utils/store/uiStore';
+import cookieBarStore from 'utils/store/cookieBarStore';
 
 const stores = {
   userStore,
   userProfileStore,
   uiStore,
   cookieBarStore,
+  siteStore,
 };
 
 export default stores;
