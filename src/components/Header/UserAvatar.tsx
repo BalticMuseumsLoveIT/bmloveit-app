62ee71d21d2f752e0321c03e1c0a0692b9004f13
Michał Zbytniewski <michal@zbytniewski.com>
2020-02-04 16:32:47 +0100

    Link avatar to profile page

--- src/components/Header/UserAvatar.tsx
+++ src/components/Header/UserAvatar.tsx
@@ -1,28 +1,36 @@
-import UserAvatarImage from 'components/Header/UserAvatar.style';
+import {
+  UserAvatarImage,
+  UserAvatarLink,
+} from 'components/Header/UserAvatar.style';
 import { getPrivateMediaURL } from 'utils/helpers';
 import { UserProfileStore } from 'utils/store/userProfileStore';
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 
-interface InjectedUserAvatarProps {
+interface Props extends RouteComponentProps {}
+
+interface InjectedProps extends Props {
   userProfileStore: UserProfileStore;
 }
 
 @inject('userProfileStore')
 @observer
-class UserAvatar extends React.Component {
+class UserAvatar extends React.Component<Props> {
   get injected() {
-    return this.props as InjectedUserAvatarProps;
+    return this.props as InjectedProps;
   }
 
   render() {
     return this.injected.userProfileStore.userHasAvatar ? (
-      <UserAvatarImage
-        src={getPrivateMediaURL(this.injected.userProfileStore.userAvatarURL)}
-        alt={this.injected.userProfileStore.userAvatarName}
-      />
+      <UserAvatarLink to="/profile">
+        <UserAvatarImage
+          src={getPrivateMediaURL(this.injected.userProfileStore.userAvatarURL)}
+          alt={this.injected.userProfileStore.userAvatarName}
+        />
+      </UserAvatarLink>
     ) : null;
   }
 }
 
-export default UserAvatar;
+export default withRouter(UserAvatar);
