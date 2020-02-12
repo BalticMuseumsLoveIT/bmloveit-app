a7c883e6640c52286fb3e8b811bad071440da3c0
piotr.zielinski <piotr.zielinski@sidnet.info>
2020-02-12 23:38:23 +0100

    Update Footer.style.tsx

--- src/components/Footer/Footer.style.tsx
+++ src/components/Footer/Footer.style.tsx
@@ -4,10 +4,11 @@ const StyledWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: fixed;
+  position: sticky;
   left: 0;
   bottom: 0;
   width: 100%;
+  z-index: 2;
 `;
 
 export default StyledWrapper;
