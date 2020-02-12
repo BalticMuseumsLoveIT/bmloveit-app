bb4e14c552aa133d0e5c15dadb2116dd9ca0daa5
piotr.zielinski <piotr.zielinski@sidnet.info>
2020-02-12 14:23:38 +0100

    Update theme.ts

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,6 +1,7 @@
 import { ThemeType } from 'utils/interfaces';
 import { RecursivePartial } from 'utils/helpers';
 import { DefaultTheme } from 'styled-components';
+import { darken, desaturate, lighten } from 'polished';
 
 const defaultType = ThemeType.LIGHT;
 
@@ -12,6 +13,10 @@ const defaultFonts = {
   subheader: {
     fontFamily: 'Montserrat, sans-serif',
     fontWeight: '600',
+  },
+  alternative: {
+    fontFamily: 'Montserrat, sans-serif',
+    fontWeight: '400',
   },
   paragraph: {
     fontFamily: '"Source Sans Pro", sans-serif',
@@ -27,8 +32,8 @@ const defaultColors = {
     anchor: {
       link: '#2A2C3E',
       hover: '#F85705',
-      active: '#2A2C3E',
-      visited: '#2A2C3E',
+      active: '#ED4200',
+      visited: '#ED4200',
     },
   },
   background: {
@@ -36,6 +41,7 @@ const defaultColors = {
     default: '#FFFFFF',
     alternative: '#F85705',
     menu: '#F8FAFD',
+    placeholder: desaturate(0.1, darken(0.04, '#EAEEF6')),
   },
   button: {
     primary: {
@@ -125,6 +131,12 @@ const defaultColors = {
     },
     info: '#7A7D92',
   },
+  icon: {
+    normal: '#7B7D92',
+    hover: '#ED591D',
+    pressed: '#BB4312',
+    inactive: '#BBBFCD',
+  },
 };
 
 export const defaultTheme: DefaultTheme = {
@@ -162,115 +174,7 @@ export const defaultTheme: DefaultTheme = {
   },
 };
 
-export const lightPartial: RecursivePartial<DefaultTheme> = {
-  colors: {
-    text: {
-      paragraph: '#7A7D92',
-      header: '#2A2C3E',
-      alternative: '#F8FAFD',
-      anchor: {
-        link: '#2A2C3E',
-        hover: '#F85705',
-        active: '#2A2C3E',
-        visited: '#2A2C3E',
-      },
-    },
-    background: {
-      app: '#EAEEF6',
-      default: '#FFFFFF',
-      alternative: '#F85705',
-      menu: '#F8FAFD',
-    },
-    button: {
-      primary: {
-        default: {
-          text: '#FFFFFF',
-          background: '#F85705',
-        },
-        hover: {
-          text: '#FFFFFF',
-          background: '#DB4F08',
-        },
-        focus: {
-          text: '#FFFFFF',
-          background: '#C34100',
-        },
-        disabled: {
-          text: '#BABFCD',
-          background: '#DCE2ED',
-        },
-      },
-      secondary: {
-        default: {
-          text: '#7A7D92',
-          background: '#FFFFFF',
-        },
-        hover: {
-          text: '#DB4F08',
-          background: '#F8F8F8',
-        },
-        focus: {
-          text: '#C34100',
-          background: '#F8F8F8',
-        },
-        disabled: {
-          text: '#BABFCD',
-          background: '#DCE2ED',
-        },
-      },
-      outline: {
-        default: {
-          text: '#7A7D92',
-          background: 'transparent',
-        },
-        hover: {
-          text: '#FFFFFF',
-          background: 'transparent',
-        },
-        focus: {
-          text: '#FFFFFF',
-          background: 'transparent',
-        },
-        disabled: {
-          text: '#BABFCD',
-          background: 'transparent',
-        },
-      },
-    },
-    list: {
-      border: '#EAEEF6',
-      header: {
-        default: {
-          text: '#2A2C3E',
-          background: '#F8FAFD',
-        },
-        hover: {
-          text: '#2A2C3E',
-          background: '#F8FAFD',
-        },
-        focus: {
-          text: '#F85705',
-          background: '#F8FAFD',
-        },
-      },
-      item: {
-        default: {
-          text: '#2A2C3E',
-          background: '#FFFFFF',
-        },
-        hover: {
-          text: '#2A2C3E',
-          background: '#F8FAFD',
-        },
-        focus: {
-          text: '#F85705',
-          background: '#F8FAFD',
-        },
-      },
-      info: '#7A7D92',
-    },
-  },
-};
+export const lightPartial: RecursivePartial<DefaultTheme> = {};
 
 export const darkPartial: RecursivePartial<DefaultTheme> = {
   colors: {
@@ -280,15 +184,17 @@ export const darkPartial: RecursivePartial<DefaultTheme> = {
       alternative: '#FFFFFF',
       anchor: {
         link: '#F8FAFD',
-        hover: '#FFFFFF',
-        active: '#F8FAFD',
-        visited: '#F8FAFD',
+        hover: '#65BFCB',
+        active: '#3C96A2',
+        visited: '#3C96A2',
       },
     },
     background: {
       app: '#2A2E37',
       default: '#464856',
       alternative: '#73BFCA',
+      menu: '#464856',
+      placeholder: desaturate(0.0, lighten(0.05, '#2A2E37')),
     },
     button: {
       primary: {
@@ -377,6 +283,12 @@ export const darkPartial: RecursivePartial<DefaultTheme> = {
         },
       },
       info: '#C8C9D5',
+    },
+    icon: {
+      normal: '#EAEEF6',
+      hover: '#73BFCA',
+      pressed: '#73BFCA',
+      inactive: '#7B7D92',
     },
   },
 };
