4fa4ce64302f2d055deed0743bb5026c54c6e8e7
Michał Zbytniewski <michal@zbytniewski.com>
2020-02-20 16:45:51 +0100

    Remove max width from InputContainer Increase vertical margin

--- src/components/Form/Form.style.tsx
+++ src/components/Form/Form.style.tsx
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
 import { Form as FormikForm } from 'formik';
-import { rem } from 'polished';
 
 const Form = styled(FormikForm)``;
 
 export const InputContainer = styled.div`
   width: 100%;
-  max-width: ${rem(360)};
-  margin: 1em auto;
+  margin: 2em auto;
 `;
 
 export default Form;
