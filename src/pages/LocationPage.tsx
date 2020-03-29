1f353212f867643e51eef630f6e0753575731c20
mzbytniewski <michal.zbytniewski@sidnet.info>
2020-03-29 23:17:49 +0200

    Add redirection logic

--- src/pages/LocationPage.tsx
+++ src/pages/LocationPage.tsx
@@ -1,13 +1,15 @@
 import Content from 'components/Content/Content';
-import { Title } from 'components/Page/Page.style';
-import LocationPageStore from 'utils/store/locationPageStore';
+import { Emphasize, Title } from 'components/Page/Page.style';
+import LocationPageStore, { ErrorType } from 'utils/store/locationPageStore';
 import React from 'react';
 import { WithTranslation, withTranslation } from 'react-i18next';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, Redirect } from 'react-router-dom';
 import Helmet from 'react-helmet';
+import { observer } from 'mobx-react';
 
 interface Props extends WithTranslation, RouteComponentProps<{ id: string }> {}
 
+@observer
 class LocationPage extends React.Component<Props> {
   locationPageStore = new LocationPageStore(true);
 
@@ -17,9 +19,13 @@ class LocationPage extends React.Component<Props> {
     } = this.props.match;
 
     this.locationPageStore.setTReady(this.props.tReady);
+
+    await this.locationPageStore.loadData(
+      /^\d+$/.test(id) ? Number.parseInt(id) : NaN,
+    );
   };
 
-  render = () => {
+  render() {
     if (!this.props.tReady) return null;
 
     return (
@@ -29,10 +35,51 @@ class LocationPage extends React.Component<Props> {
         </Helmet>
         <Content>
           <Title>{this.props.t('content.title', 'Location')}</Title>
+
+          {this.locationPageStore.itemFound && (
+            <Redirect
+              to={{
+                pathname: `/item/${this.locationPageStore.itemId}`,
+              }}
+            />
+          )}
+
+          {this.locationPageStore.hasError && (
+            <Emphasize>
+              <p>
+                {(() => {
+                  switch (this.locationPageStore.errorType) {
+                    case ErrorType.INVALID_ID:
+                      return this.props.t(
+                        'error.location.invalidId',
+                        'Sorry, provided location ID is invalid',
+                      );
+                    case ErrorType.LOCATION_NOT_FOUND:
+                      return this.props.t(
+                        'error.location.invalidId',
+                        'Sorry, location with a given ID was not found',
+                      );
+                    case ErrorType.LOCATION_HAS_NO_DEFAULT_ITEM:
+                      return this.props.t(
+                        'error.location.invalidId',
+                        'Sorry, location with a given ID has no default item',
+                      );
+                    case ErrorType.GENERIC:
+                      return this.props.t(
+                        'error.location.generic',
+                        'Sorry, item id could not be determined',
+                      );
+                    default:
+                      return null;
+                  }
+                })()}
+              </p>
+            </Emphasize>
+          )}
         </Content>
       </>
     );
-  };
+  }
 }
 
 export default withTranslation('location-page')(LocationPage);
