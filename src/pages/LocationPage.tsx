d9476c7df0c70b9591375647532d215bd4f604b5
mzbytniewski <michal.zbytniewski@sidnet.info>
2020-03-29 21:03:35 +0200

    Use store

--- src/pages/LocationPage.tsx
+++ src/pages/LocationPage.tsx
@@ -1,7 +1,23 @@
+import Content from 'components/Content/Content';
+import { Title } from 'components/Page/Page.style';
+import LocationPageStore from 'utils/store/locationPageStore';
 import React from 'react';
 import { WithTranslation, withTranslation } from 'react-i18next';
+import { RouteComponentProps } from 'react-router-dom';
+import Helmet from 'react-helmet';
 
-interface Props extends WithTranslation {}
+interface Props extends WithTranslation, RouteComponentProps<{ id: string }> {}
+
+class LocationPage extends React.Component<Props> {
+  locationPageStore = new LocationPageStore(true);
+
+  componentDidMount = async () => {
+    const {
+      params: { id },
+    } = this.props.match;
+
+    this.locationPageStore.setTReady(this.props.tReady);
+  };
 
 class LocationPage extends React.Component<Props> {}
 
